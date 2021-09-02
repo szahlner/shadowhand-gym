29f9f2b203f3e3998734ac7c9d303b237e205c22
szahlner <stefan.zahlner@gmx.at>
2021-09-02 19:48:08 +0200

    add difficult mode to environment-id

--- shadowhand_gym/__init__.py
+++ shadowhand_gym/__init__.py
@@ -5,12 +5,19 @@ for reward_type in ["dense", "sparse"]:
     suffix = "Dense" if reward_type == "dense" else ""
     kwargs = {"reward_type": reward_type}
 
-    register(
-        id="ShadowHandReach{}-v1".format(suffix),
-        entry_point="shadowhand_gym.envs:ShadowHandReachEnv",
-        kwargs=kwargs,
-        max_episode_steps=50,
-    )
+    for difficult_mode in ["easy", "hard"]:
+        difficult_mode = "Hard" if difficult_mode == "hard" else ""
+        kwargs["difficult_mode"] = difficult_mode
+
+        register(
+            id="ShadowHandReach{}-v1".format(suffix),
+            entry_point="shadowhand_gym.envs:ShadowHandReachEnv",
+            kwargs=kwargs,
+            max_episode_steps=50,
+        )
+
+    # Unset the difficult mode
+    kwargs.pop("difficult_mode", None)
 
     register(
         id="ShadowHandBlock{}-v1".format(suffix),
